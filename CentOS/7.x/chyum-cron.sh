92634263363b51823c49920824035970d5ff9579
patrickpan <Patrick-SQ.Pan@aia.com>
2018-06-14 11:36:16 +0800

    Change Update Cmd to be Default
    1.According to https://www.centos.org/forums/viewtopic.php?f=47&t=51300, "yum --security check-update" is useless on CentOS 7.

--- CentOS/7.x/chyum-cron.sh
+++ CentOS/7.x/chyum-cron.sh
@@ -20,9 +20,9 @@ echo "#$0" >> $1
 ##################################
 # Specify the Category of Update #
 ##################################
-echo "$0: sed -i 's/update_cmd = default/#update_cmd = default/g' $1"
-sed -i "s/update_cmd = default/#update_cmd = default/g" $1
-echo "update_cmd = security" >> $1
+#echo "$0: sed -i 's/update_cmd = default/#update_cmd = default/g' $1"
+#sed -i "s/update_cmd = default/#update_cmd = default/g" $1
+#echo "update_cmd = security" >> $1
 
 #################
 # Apply Updates #
