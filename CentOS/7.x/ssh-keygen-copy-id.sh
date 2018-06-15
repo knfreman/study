a969346976da3ba1ed4bfd49924c492ab777131f
patrickpan <Patrick-SQ.Pan@aia.com>
2018-06-15 12:17:48 +0800

    Update ssh-keygen-copy-id.sh
    1.Use getOpts to get params.
    2.If username is unspecified, current username will be used.
    3.If port is unspecified, 22 will be used.
    4.If filename is unpsecified, hostname will be used.

--- CentOS/7.x/ssh-keygen-copy-id.sh
+++ CentOS/7.x/ssh-keygen-copy-id.sh
@@ -1,23 +1,66 @@
 #!/bin/bash
 
-if [ $# -lt 3 ] ; then
+if [ $# -eq 0 ] ; then
 	echo "This script is used for generating ssh key and upload it to the server."
-	echo "Usage: sh $0 [filename] [port] [username] [hostname or IP Address], e.g., $0 id_rsa 22 Patrick 123.123.123.123."
+	echo "Usage: sh $0 -f [filename] -p [port] -u [username] -h [hostname or IP Address]." 
+	echo "e.g., sh $0 -f id_rsa -p 33333 -u Patrick -h 123.123.123.123"
 	exit 0
 fi
 
-echo "$0: [filename] is $1, [port] is $2 [username] is $3 and [hostname or IP address] is $4."
+while getopts "f:p:u:h:" opt  
+do  
+    case $opt in  
+        f)  
+        filename=$OPTARG
+        ;;  
+        p)  
+        port=$OPTARG
+        ;;  
+        u)  
+        username=$OPTARG
+        ;;  
+        h)  
+        hostname=$OPTARG
+        ;;  
+        ?)   
+        ;;  
+    esac  
+done  
+
+echo "[hostname] is $hostname"
+
+if [ ! $hostname ] ; then
+	echo "[hostname] is unspecified!"
+	exit 0
+fi
+
+if [ ! $username ] ; then
+	username=$(whoami)
+fi
+
+if [ ! $filename ] ; then
+	filename=$hostname
+fi
+
+if [ ! $port ] ; then
+	port=22
+fi
+
+echo "$0: filename=$filename"
+echo "$0: port=$port"
+echo "$0: username=$username"
+echo "$0: hostname=$hostname"
 
 ####################
 # Generate SSH Key #
 ####################
-echo "$0: ssh-keygen -P '' -f ~/.ssh/$1"
-ssh-keygen -P '' -f ~/.ssh/$1
+echo "$0: ssh-keygen -P '' -f ~/.ssh/$filename"
+ssh-keygen -P '' -f ~/.ssh/$filename
 
 ############################
 # Upload SSH Key to Server #
 ############################
-echo "$0: ssh-copy-id -i ~/.ssh/$1.pub -p $2 $3@$4"
-ssh-copy-id -i ~/.ssh/$1.pub -p $2 $3@$4
+echo "$0: ssh-copy-id -i ~/.ssh/$filename.pub -p $port $username@$hostname"
+ssh-copy-id -i ~/.ssh/$filename.pub -p $port $username@$hostname
 
 echo "$0: Complete!"
