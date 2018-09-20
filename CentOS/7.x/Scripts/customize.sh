a1acf223b67143be85b2b7489f0a3d6363d8c2e0
patrickpan <Patrick-SQ.Pan@aia.com>
2018-09-20 15:00:16 +0800

    Upload customize.sh for CentOS 7.x

---
+++ CentOS/7.x/Scripts/customize.sh
@@ -0,0 +1,20 @@
+backupFile(){
+	echo "$0: cp -f $1 $1.bak_\`date +\"%Y-%m-%d_%H:%M:%S\"\`"
+	cp -f $1 $1.bak_`date +"%Y-%m-%d_%H:%M:%S"`
+}
+
+##################################
+ # Show Line Number in Vi and Vim #
+##################################
+
+appendSetNumber(){
+	backupFile $2
+	echo "" >> $2
+	echo $1 >> $2
+	echo "set number" >> $2
+}
+
+appendSetNumber "\" $0" "/etc/virc"
+appendSetNumber "\" $0" "/etc/vimrc"
+
+ 
