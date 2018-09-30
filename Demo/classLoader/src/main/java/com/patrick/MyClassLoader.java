1ea22458cd4cd4196d37457d70595b92a925656c
patrickpan <Patrick-SQ.Pan@aia.com>
2018-09-30 19:44:57 +0800

    Refactor Codes
    1.build.gradle: use variable to represent "MyObject".
    2.MyClassLoader.java: use variable instead of hardcode.

--- Demo/classLoader/src/main/java/com/patrick/MyClassLoader.java
+++ Demo/classLoader/src/main/java/com/patrick/MyClassLoader.java
@@ -11,18 +11,23 @@ import java.io.InputStream;
 public class MyClassLoader extends ClassLoader {
 
 	@Override
-	public Class<?> findClass(String classFile) throws ClassNotFoundException {
+	public Class<?> findClass(String className) throws ClassNotFoundException {
 		try {
-			byte[] bytes = getBytes();
-			return this.defineClass(classFile, bytes, 0, bytes.length);
+			byte[] bytes = getBytes(getClassFileName(className));
+			return this.defineClass(className, bytes, 0, bytes.length);
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new ClassNotFoundException("Exception occurs in MyClassLoader.findClass.");
 		}
 	}
 
-	private byte[] getBytes() throws IOException {
-		try (InputStream inputStream = MyClassLoader.class.getClassLoader().getResourceAsStream("MyObject")) {
+	private String getClassFileName(String className) {
+		int index = className.lastIndexOf(".") + 1;
+		return className.substring(index);
+	}
+
+	private byte[] getBytes(String classFileName) throws IOException {
+		try (InputStream inputStream = MyClassLoader.class.getClassLoader().getResourceAsStream(classFileName)) {
 			int len = inputStream.available();
 			byte[] bytes = new byte[len];
 			inputStream.read(bytes, 0, len);
