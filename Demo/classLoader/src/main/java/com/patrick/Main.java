69d4fe73de4ce6b541b0d13aa518121416404db2
patrickpan <Patrick-SQ.Pan@aia.com>
2018-09-30 21:35:16 +0800

    Show Differences between "loadClass" and "findClass"

--- Demo/classLoader/src/main/java/com/patrick/Main.java
+++ Demo/classLoader/src/main/java/com/patrick/Main.java
@@ -2,8 +2,11 @@ package com.patrick;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		// Class<?> clazz = new MyClassLoader().loadClass("com.patrick.MyObject");
 		Class<?> clazz = new MyClassLoader().findClass("com.patrick.MyObject");
 		Object obj = clazz.newInstance();
 		obj = (MyObject) obj;
+
+		System.out.print("Hello, world!");
 	}
 }
